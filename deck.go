159dc0cdb41d6dce62d2e230baf00bab46d450e4
AndrewTtofi <andreas@ttofi.com>
2022-11-03 21:06:03 +0200

    additions

--- deck.go
+++ deck.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
-type deck []string
+type deck []string //create a new type of deck that will be basically a slice of type string
 
 func newDeck() deck {
-	cards := deck{}
+	cards := deck{} //Initialize the cards slice of type deck
 	cardtypes := []string{"Spades", "Hearts", "Clubs", "Diamonds"}
 	cardnumbers := []string{"Ace", "Two", "Three", "Four"}
 	for _, cardtype := range cardtypes {
@@ -20,4 +24,25 @@ func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",") //transform the slice into a single line string and set the delimeter "," that wil seperate the value of the slice
+}
+
+func (d deck) saveToFile(filename string) error {
+	return os.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename) //bs stands for byteslice and will be the value of the file
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	return strings.Split(string(bs), ",")
 }
--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
+	// // cards.print()
+	// hand, remainingDeck := deal(cards, 4)
+	// hand.print()
+	// remainingDeck.print()
+	// cards.print()
+	// cards.saveToFile("giannakis")
+	cards := newDeckFromFile("giannakis")
 	cards.print()
 }
