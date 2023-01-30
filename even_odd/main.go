9a000b43c785eceeaeadc63476c5b476f4559fce
Andrew Ttofi <andreas@ttofi.com>
2023-01-30 17:01:22 +0200

    add even odd exercise

--- even_odd/main.go
+++ even_odd/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n []int
+	for i := 0; i < 11; i++ {
+		n = append(n, i)
+	}
+	for i := range n {
+		if n[i]%2 == 0 {
+			fmt.Println("The number ", n[i], " is even")
+		} else {
+			fmt.Println("The number ", n[i], " is odd")
+		}
+	}
+}
