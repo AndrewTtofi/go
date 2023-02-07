9400e217df6061682b92ab0fe1ee49530f06b0cf
Andrew Ttofi <andreas@ttofi.com>
2023-02-07 10:33:05 +0200

    maps intro

--- maps/main.go
+++ maps/main.go
@@ -1,1 +1,25 @@
-package maps
+package main
+
+import "fmt"
+
+func main() {
+	/*alternative methods to declare a map
+	var colors map[string]string
+	colors := make(map[string]string)
+	*/
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00FF00",
+	}
+	//adding an additional value in the map
+	colors["white"] = "#FFFFFF"
+	//delete a value from a map
+	delete(colors, "red")
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex for ", color, "is", hex)
+	}
+}
